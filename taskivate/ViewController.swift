af44640c33ef1f23da4c674a9798f5dc6c17737f
Neil baron <neilcb71@hotmail.com>
2018-05-02 20:24:41 -0700

    Initial Commit

---
+++ taskivate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  taskivate
+//
+//  Created by Neil Baron on 5/2/18.
+//  Copyright © 2018 taskivate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
