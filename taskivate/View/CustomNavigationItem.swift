dfce40564eac27b1d25325311a634f62deca19da
Neil baron <neilcb71@hotmail.com>
2018-09-17 10:59:30 -0500

    Updated refresh tasks function

---
+++ taskivate/View/CustomNavigationItem.swift
@@ -0,0 +1,21 @@
+//
+//  CustumNavigationItem.swift
+//  taskivate
+//
+//  Created by Neil Baron on 8/11/18.
+//  Copyright © 2018 taskivate. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavigationItem: UINavigationItem {
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+    
+    
+
+}
